514f3eccd9ccb2832269033ecc85f22f3e8ff689
HiddenBad <thibault.havet66@gmail.com>
2026-08-09 10:00:41 +0200

    Adding readme, license, and vsix

---
+++ README.md
@@ -0,0 +1,13 @@
+## odoo-helper
+
+### Features
+
+- Run test under cursor, if the cursor is in the body of a python test method, it will run this test.
+Describe specific features of your extension including screenshots of your extension in action.
+
+### Extension Settings
+
+This extension contributes the following settings:
+
+* `odoo-helpers.run-python-unit-test`: Run the test under the cursor.
+
--- extension.js
+++ extension.js
@@ -17,7 +17,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('odoo-helpers.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('odoo-helpers.run-python-unit-test', function () {
 		let activeTerminal = vscode.window.activeTerminal;
 		if (!activeTerminal) {
 			activeTerminal = vscode.window.createTerminal();
