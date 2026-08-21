fc581325d6d99fb4c9f1c5d3b441608cb0462ac1
HiddenBad <thibault.havet66@gmail.com>
2026-08-21 17:12:41 +0200

    Adding Move To Setup

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 ### Features
 
-- Run test under cursor, if the cursor is in the body of a python test method, it will run this test.
-Describe specific features of your extension including screenshots of your extension in action.
+- Run test under cursor, if the cursor is in the body of a python test method, it will run this test using the `o` script.
+- Show the active file history in the console using `tig blame`.
+- Move the variable under the cursor to the setupClass of a test class. You'll be prompted the name of the variable.
 
 ### Extension Settings
 
 This extension contributes the following settings:
 
 * `odoo-helpers.run-python-unit-test`: Run the test under the cursor.
-
+* `odoo-helpers.run-tig-blame`: Run `tig blame` on the active file.
+* `odoo-helpers.python-move-var-to-setup`: Move the variable under the cursor to the setupClass method.
--- extension.js
+++ extension.js
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 const vscode = require('vscode');
 const common = require('./common');
+const pythonParser = require('./python_parser');
 
 const startsWith = common.startsWith;
 
@@ -11,7 +12,10 @@ const startsWith = common.startsWith;
 function activate(context) {
 	const disposable1 = vscode.commands.registerCommand('odoo-helpers.run-python-unit-test', runPythonUnitTest);
 	const disposable2 = vscode.commands.registerCommand('odoo-helpers.run-tig-blame', runTigBlame);
-	context.subscriptions.push(disposable1, disposable2);
+	const disposable3 = vscode.commands.registerCommand('odoo-helpers.python-move-var-to-setup', runMoveToSetup);
+	const disposable4 = vscode.commands.registerCommand('odoo-helpers.python-replace-date', runReplaceDateUnderCursor);
+	const disposable5 = vscode.commands.registerCommand('odoo-helpers.python-select-expand-copy', runSelectExpandCopy);
+	context.subscriptions.push(disposable1, disposable2, disposable3, disposable4, disposable5);
 }
 
 function runPythonUnitTest() {
@@ -25,17 +29,17 @@ function runPythonUnitTest() {
 	let line = selectionPosition.line;
 	while (line >= 0) {
 		const lineText = activeTextEditor.document.lineAt(line).text;
-		if (startsWith(lineText, '    def ')) {
+		if (startsWith(lineText.trimStart(), 'def ')) {
 			if (startsWith(lineText.substring(8), 'test_')) {
 				const test_method_name = lineText.substring(8).split('(', 1)
 				const activeTerminal = common.getActiveOrCreateTerminal();
 				activeTerminal.sendText('o -t .' + test_method_name);
-				activeTerminal.show();
+				// TODO: add settings to decide whether to show the terminal
 				return;
 			} else {
 				vscode.window.showWarningMessage('The cursor is not in the body of a test !');
 				return;
-			}
+			}	
 		}
 		line--;
 	}
@@ -47,22 +51,161 @@ function runTigBlame() {
 		vscode.window.showWarningMessage(activeTextEditor['reason']);
 		return;
 	}
-	activeTextEditor = activeTextEditor['result'];
+	const activeTextEditorFilePath = activeTextEditor['result'].document.uri.path;
 
 	let workspaceFolder = common.getWorkspaceFolder();
 	if (workspaceFolder['errorResult']){
 		vscode.window.showWarningMessage(workspaceFolder['reason']);
 		return;
 	}
-	workspaceFolder = workspaceFolder['result']
-	if (!startsWith(activeTextEditor.document.uri.path, workspaceFolder.uri.path)) {
+	const workspaceFolderPath = workspaceFolder['result'].uri.path;
+	if (!startsWith(activeTextEditorFilePath, workspaceFolderPath)) {
 		vscode.window.showWarningMessage('The focused editor is related to a new file, or to a file that doesn\'t belong to the project!');
 		return;
 	}
-	const fileRelativePath = activeTextEditor.document.uri.path.substring(workspaceFolder.uri.path.length + 1);
+	const fileRelativePath = activeTextEditorFilePath.substring(workspaceFolderPath.length + 1);
 	const activeTerminal = common.getActiveOrCreateTerminal();
 	activeTerminal.sendText('o tblame ' + fileRelativePath);
 	activeTerminal.show();
+}
+
+async function runMoveToSetup() {
+	const nActiveTextEditor = common.getActivePythonTextEditor();
+	if (nActiveTextEditor['errorResult']) {
+		vscode.window.showWarningMessage(nActiveTextEditor['reason']);
+		return;
+	}
+	const activeTextEditor = nActiveTextEditor.result;
+	const text = activeTextEditor.document.getText();
+
+	// Find the start and end positions of the setUpClass(cls) function
+	const setUpClassRegex = /def setUpClass\(cls\):(?:\s|.)*?(?=\n    def |\nclass |\n$)/g;
+	const setupClassMatch = setUpClassRegex.exec(text);
+
+	if (!setupClassMatch) {
+		vscode.window.showInformationMessage('No setUpClass(cls) function found!');
+		return;
+	}
+
+	try {
+		let assignmentStartEnd = pythonParser.getAssignmentStartEnd(activeTextEditor);
+		const firstAssignmentLineIndent = pythonParser.getIndent(activeTextEditor.document.lineAt(assignmentStartEnd.startLine.line).text);
+		activeTextEditor.selection = new vscode.Selection(assignmentStartEnd.startLine, assignmentStartEnd.endLine);
+		const userInputName = await vscode.window.showInputBox();
+
+		const setUpClassText = setupClassMatch[0];
+		const setUpClassStartIndex = setupClassMatch.index;
+		const setUpClassEndIndex = setUpClassStartIndex + setUpClassText.length;
+
+		const firstSetupInstruction = setUpClassText.split('\n')[1]
+		const setupIndent = pythonParser.getIndent(firstSetupInstruction)
+
+		const lastLineStart = activeTextEditor.document.positionAt(setUpClassEndIndex - 1);
+		const lastLine = activeTextEditor.document.lineAt(lastLineStart.line);
+
+		const assignmentValue = activeTextEditor.document.getText(activeTextEditor.selection);
+		activeTextEditor.edit((editBuilder) => {
+			// Edit-Remove assignment lines
+			const selection_start = activeTextEditor.selection.start;
+			editBuilder.delete(activeTextEditor.selection);
+			editBuilder.insert(selection_start, `self.${userInputName}`)
+
+			// Insert the text in the setupClass
+            let insertPosition = new vscode.Position(lastLine.lineNumber, lastLine.text.length);
+			const assignmentLines = assignmentValue.split('\n');
+			let assignmentNewLine = `\n${' '.repeat(setupIndent)}cls.${userInputName} = ${assignmentLines[0]}`
+			let assignmentEntireText = assignmentNewLine;
+			for (let i = 1; i < assignmentLines.length; i++) {
+				let assignmentLineIndent = pythonParser.getIndent(assignmentLines[i]);
+				let lineRelativIndent = assignmentLineIndent - firstAssignmentLineIndent;
+				let newIndent = setupIndent + lineRelativIndent;
+				let indentDiff = assignmentLineIndent - newIndent;
+				if (indentDiff < 0) {
+					assignmentNewLine = `\n${' '.repeat(-indentDiff)}${assignmentLines[i]}`;
+				} else if (indentDiff > 0) {
+					assignmentNewLine = `\n${assignmentLines[i].slice(indentDiff)}`;
+				} else {
+					assignmentNewLine = `\n${assignmentLines[i]}`;
+				}
+				assignmentEntireText += assignmentNewLine
+			}
+			editBuilder.insert(insertPosition, assignmentEntireText);
+        });
+	} catch (e) {
+		vscode.window.showWarningMessage(e.message);
+	}
+}
+
+function runReplaceDateUnderCursor() {
+	const nActiveTextEditor = common.getActivePythonTextEditor();
+	if (nActiveTextEditor['errorResult']) {
+		vscode.window.showWarningMessage(nActiveTextEditor['reason']);
+		return;
+	}
+
+	const activeTextEditor = nActiveTextEditor.result;
+	const cursorPosition = activeTextEditor.selection.active;
+	const lineText = activeTextEditor.document.lineAt(cursorPosition.line).text;
+	const cursorChar = cursorPosition.character;
+
+	let quoteStart = -1;
+	let quoteChar = '';
+	for (let i = cursorChar; i >= 0; i--) {
+		if (lineText[i] === '\'' || lineText[i] === '"') {
+			quoteStart = i;
+			quoteChar = lineText[i];
+			break;
+		}
+	}
+
+	if (quoteStart === -1) {
+		vscode.window.showWarningMessage('The cursor is not on a date!');
+		return;
+	}
+
+	const quoteEnd = lineText.indexOf(quoteChar, quoteStart + 1);
+	if (quoteEnd === -1 || cursorChar > quoteEnd) {
+		vscode.window.showWarningMessage('The cursor is not on a date!');
+		return;
+	}
+
+	const candidate = lineText.slice(quoteStart + 1, quoteEnd);
+	const dateMatch = /^(\d{4})-(\d{2})-(\d{2})$/.exec(candidate);
+	if (!dateMatch) {
+		vscode.window.showWarningMessage('The cursor is not on a date!');
+		return;
+	}
+
+	const replacement = `date(${parseInt(dateMatch[1], 10)}, ${parseInt(dateMatch[2], 10)}, ${parseInt(dateMatch[3], 10)})`;
+	const range = new vscode.Range(cursorPosition.line, quoteStart, cursorPosition.line, quoteEnd + 1);
+	activeTextEditor.edit((editBuilder) => {
+		editBuilder.replace(range, replacement);
+	});
+
+}
+
+const string_del = ['\'', '"'];
+const openingCharsSet = ['{', '[', '('];
+const closingCharsSet = ['}', ']', ')'];
+
+function runSelectExpandCopy() {
+	const nActiveTextEditor = common.getActivePythonTextEditor();
+	if (nActiveTextEditor['errorResult']) {
+		vscode.window.showWarningMessage(nActiveTextEditor['reason']);
+		return;
+	}
+
+	const activeTextEditor = nActiveTextEditor.result;
+	const cursorPosition = activeTextEditor.selection.active;
+	const lineText = activeTextEditor.document.lineAt(cursorPosition.line).text;
+	let start_left_pos = cursorPosition.character;
+	if (start_left_pos > 0) {
+		start_left_pos--;
+	}
+	for (let i = start_left_pos; i >= 0; i--) {
+		const char = lineText.at(i);
+		if (string_del.includes(char) || openingCharsSet.includes(char))
+	}
 }
 
 // This method is called when your extension is deactivated
